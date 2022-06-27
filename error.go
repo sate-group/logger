48db16e1bde3a79b6e030e4dcda50eb6196e3560
welcomehyunseo <welcomehyunseo@gmail.com>
2022-06-27 19:52:16 +0900

    add Error func for only one error

--- error.go
+++ error.go
@@ -6,3 +6,8 @@ func Errorf(format string, v ...any) {
 	format = string(COLOR_RED) + "ERROR: " + format + string(COLOR_RESET)
 	log.Printf(format, v...)
 }
+
+func Error(err error) {
+	format := string(COLOR_RED) + "ERROR: " + err.Error() + string(COLOR_RESET)
+	log.Print(format)
+}
